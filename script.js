95691998d50318d935450afa8de78721a9f7b944
Marco <mcg.tampieri@gmail.com>
2018-12-12 16:58:46 +0100

    finito +/-

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-//XXXXXXXXXXXXXXXXXXXXXXXXXX GET ALL REQUEST XXXXXXXXXXXXXXXXXXXXXXXXXX
-
 let getUsers = document.querySelector(".getUsers");
+let getUserById = document.querySelector(".getUserById");
+let postUser = document.querySelector(".postUser");
+let updateUser = document.querySelector(".updateUser");
+let deleteUser = document.querySelector(".deleteUser");
+
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//  GET REQUEST
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 let displayUsers = (data) => {
 
@@ -21,7 +28,6 @@ let displayUsers = (data) => {
         `
         usersList.appendChild(li);
     })
-
 }
 
 let showUsers = () => {
@@ -30,17 +36,14 @@ let showUsers = () => {
     fetch(url)
         .then(response => response.json())
         .then(parsedData => displayUsers(parsedData))
-        .catch((error) => console.warn(error))
+        .catch((error) => console.warn(error))    
 }
 
-getUsers.addEventListener("click", showUsers)
-
-//XXXXXXXXXXXXXXXXXXXXXXXXXX GET BY ID REQUEST XXXXXXXXXXXXXXXXXXXXXXXXXX
-
-let getUserById = document.querySelector(".getUserById");
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//  GET BY ID REQUEST
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 let displaySingleUser = (x) => {
-
     let singleUser = document.querySelector(".singleUser");
 
     while (singleUser.hasChildNodes()) {
@@ -64,33 +67,90 @@ let showUsersParam = (id) => {
         .then(response => response.json())
         .then(parsedData => displaySingleUser(parsedData))
         .catch((error) => console.warn(error))
+    document.querySelector(".inputID").value = ""
 }
 
-getUserById.addEventListener("click", function (inputValue) {
-     inputValue = document.querySelector(".inputID").value;
-    showUsersParam(inputValue);
-})
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//  POST REQUEST
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-//XXXXXXXXXXXXXXXXXXXXXXXXXX POST REQUEST XXXXXXXXXXXXXXXXXXXXXXXXXX
-
-let postNew = document.querySelector(".postNew");
-
-let postUser = (data) => {
-    console.log(data)
-}
-
-let enterPost = () => {
-    let url = "http://10.20.0.214:2525/watch";
+let enterPost = (e) => {
+    e.toString();
+    let url = "https://watchmanager.herokuapp.com/student";
 
     fetch(url, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json; charset=utf-8",
-            }
+            },
+            body: JSON.stringify({
+                "studentName" : e
+            })
         })
-        .then(response => response.json())
-        .then(parsedData => postUser(parsedData))
-        .catch((error) => console.warn(error))
+    document.querySelector(".funny").value = ";"
 }
 
-postNew.addEventListener("click", enterPost)
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//  UPDATE REQUEST
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+let sendUpdates = (e) => {
+    
+    let newName = document.querySelector(".updateName");
+    let newSubject = document.querySelector(".updateSubject");
+    let newDate = document.querySelector(".updateDate");
+    let userID = document.querySelector(".userID");
+
+    let url = `https://watchmanager.herokuapp.com/student/${e}`;
+
+    fetch(url, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        },
+        body: JSON.stringify({
+            "studentName" : newName.value,
+            "nextWatchSubject": newSubject.value,
+            "nextWatchDate": newDate.value
+        })
+    })
+
+    userID.value = "";
+    newName.value = "";
+    newSubject.value = "";
+    newDate.value = "";
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//  DELETE REQUEST
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+   
+let deleteFunction =(e) => {
+    let url = `https://watchmanager.herokuapp.com/student/${e}`
+    fetch(url, {
+        method: "DELETE",
+    })
+    document.querySelector(".notFunny").value = "";
+}
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//  EVENT LISTENERS
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+getUsers.addEventListener("click", showUsers)
+getUserById.addEventListener("click", function (inputValue) {
+    inputValue = document.querySelector(".inputID").value;
+   showUsersParam(inputValue);
+})
+postUser.addEventListener("click", function (e) {
+    e = document.querySelector(".funny").value;
+    enterPost(e);
+})
+updateUser.addEventListener("click", function (e) {
+    e = document.querySelector(".userID").value;
+    sendUpdates(e)
+})
+deleteUser.addEventListener("click", function (e) {
+    e = document.querySelector(".notFunny").value;
+    deleteFunction(e);
+})
